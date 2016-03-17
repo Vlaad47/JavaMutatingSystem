e6c3b65d002cde1dbf82007a862f0a0858ef267d
Michał Mnich <michael.mnich@gmail.com>
2016-03-17 21:30:43 +0100

    poprawki

--- JavaMag/Program.cs
+++ JavaMag/Program.cs
@@ -21,14 +21,42 @@ namespace JavaMag
             Java8Parser parser = new Java8Parser(tokens);
             ParserRuleContext tree = parser.compilationUnit();
             Console.WriteLine(tree.GetText());
-            foreach (var dupa in tree.children)
-            {
-                Console.WriteLine(dupa);
-            }
-            var zenon = new Java8Parser.TypeDeclarationContext(tree, 1);
-            tree.children[1] = zenon;
-            Console.WriteLine(tree.GetText());
+            Console.WriteLine(new JavaVisitor().Visit(tree));
             Console.Read();
+        }
+    }
+
+    class JavaVisitor : Java8BaseVisitor<String>
+    {
+        public override string VisitCompilationUnit(Java8Parser.CompilationUnitContext context)
+        {
+            return base.VisitCompilationUnit(context);
+        }
+
+        public override string VisitTypeDeclaration(Java8Parser.TypeDeclarationContext context)
+        {
+            Console.WriteLine("TypeDeclaration");
+            return base.VisitTypeDeclaration(context);
+        }
+
+        public override string VisitStatement(Java8Parser.StatementContext context)
+        {
+            Console.WriteLine("Statement");
+            return base.VisitStatement(context);
+        }
+
+        public override string VisitIfThenStatement(Java8Parser.IfThenStatementContext context)
+        {
+            Console.WriteLine("IF");
+            Java8Parser.ExpressionContext zenon = context.expression();
+            //zenon.GetTokens();
+            return base.VisitIfThenStatement(context);
+        }
+
+        public override string VisitRelationalExpression(Java8Parser.RelationalExpressionContext context)
+        {
+            Console.WriteLine(3);
+            return base.VisitRelationalExpression(context);
         }
     }
 }
