382d16243ee3ecf894ec8868bbdb2b64017291e4
Michał Mnich <michael.mnich@gmail.com>
2016-03-30 15:48:43 +0200

    Naprawa braków katalogów i wersionowania

--- JavaMag/Program.cs
+++ JavaMag/Program.cs
@@ -38,7 +38,7 @@ namespace JavaMag
     class MutatorOperator
     {
         public string TooMuttate;// co chce mutowac np: <,>,!=,.....
-        public string Mutators; // <,>
+        public string Mutators; // <,> *
     }
 
     class JavaVisitor : Java8BaseVisitor<String>
@@ -48,7 +48,7 @@ namespace JavaMag
         {
             Console.WriteLine(context.GetChild(0).Payload.GetType());
             CommonToken zenon = new CommonToken((CommonToken)context.GetChild(0).Payload);
-            zenon.Text = ">";
+            zenon.Text = ">"; //random z *
             context.RemoveLastChild();
             context.AddChild(zenon);
             return base.VisitCompareOperator(context);
